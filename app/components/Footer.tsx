0792fc9fd8889112299f47dcd6364726461a26b0
hanna <hannadhm@hotmail.co.uk>
2024-07-30 14:58:59 +0100

    add footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,28 @@
-export default function Footer() {
-    return (
-      <div className="">
-        <h2>Footer</h2>
+import { Footer, FooterCopyright, FooterIcon } from "flowbite-react";
+import {
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+  BsYoutube,
+} from "react-icons/bs";
+
+export default function PageFooter() {
+  return (
+    <Footer container>
+      <div className="w-full sm:flex sm:items-center sm:justify-between">
+        <FooterCopyright href="#" by="MEGEB™" year={2024} />
+        <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+          <FooterIcon href="#" icon={BsFacebook} />
+          <FooterIcon href="#" icon={BsInstagram} />
+          <FooterIcon href="#" icon={BsTwitter} />
+          <FooterIcon
+            href="https://github.com/porlowska/megeb"
+            icon={BsGithub}
+          />
+          <FooterIcon href="#" icon={BsYoutube} />
+        </div>
       </div>
-    );
-  }
+    </Footer>
+  );
+}
