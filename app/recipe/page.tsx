314a702a51947508c6bae02f01d5be8aadcef2bc
hanna <hannadhm@hotmail.co.uk>
2024-08-10 16:00:52 +0100

    test endpoint in recipe page

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -1,55 +1,74 @@
+"use client";
+
 import NaviBar from "../components/NaviBar";
 import Footer from "../components/Footer";
 import { Button } from "flowbite-react";
 import { HiHeart } from "react-icons/hi";
-import Image from 'next/image'
+import data from "../api/endpoint-test";
+// import { getRecipeData } from "../components/SearchBar";
 
 export default function Home() {
   return (
     <section className="">
       <NaviBar />
       {/* <Return/> Return to search button compnent */}
-      <button> return to seach </button>
+      <button> return to search </button>
       {/* div witn img  */}
-        <div>
-                <Image
-            src="form data"
-            width={500}
-            height={500}
-            alt="recipe name "
-            />
-        </div>
-      {/* div with recipe */}
-      <div >
-        <h1>{"recipe name from data"}</h1>
-        <div>
-        {/* tags */}
-        <div className="flex items-center justify-between">
-          {/* iterate through its categories if possible  */}
-          <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
-            # {"Catergory"}
-          </span>
-          {/* add to favourites  */}
-          <Button outline pill>
-            <HiHeart className="h-6 w-6" />
-          </Button>
-        </div>
-    
-        <div className="flex items-center justify-between">
-         
-          <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
-            # {"Ready in"}
-          </span>
-          <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
-            # {"servings"}
-          </span>
-        </div>
-
-        <h2>Ingredients</h2>
-        {/* form data */}
-        <h2>How to:</h2>
-        {/* from data  */}
+      <div>
+        <img
+          src={`${data.recipes[0].image}`}
+          width={500}
+          height={500}
+          alt="recipe name"
+        />
       </div>
+      {/* div with recipe */}
+      <div>
+        {/* {getRecipeData()} */}
+        <h1>{data.recipes[0].title}</h1>
+        <div>
+          {/* tags */}
+
+          <div className="flex items-center justify-between">
+            {/* iterate through its categories if possible  */}
+            {data.recipes[0].dishTypes.map((type) => {
+              return (
+                <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+                  # {type}
+                </span>
+              );
+            })}
+            {/* add to favourites  */}
+            <Button outline pill>
+              <HiHeart className="h-6 w-6" />
+            </Button>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+              # {`Ready in ${data.recipes[0].readyInMinutes} minutes`}
+            </span>
+            <span className="me-2 rounded-full bg-gray-100 px-2.5 py-0.5 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
+              # {`Serves ${data.recipes[0].servings}`}
+            </span>
+          </div>
+          <h2>Ingredients</h2>
+          {data.recipes[0].extendedIngredients.map((item) => {
+            return (
+              <ul>
+                <li>{item.name}</li>
+              </ul>
+            );
+          })}
+          {/* form data */}
+          <h2>How to:</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data.recipes[0].instructions,
+            }}
+          ></div>
+          {/* from data  */}
+        </div>
       </div>
       <Footer />
     </section>
