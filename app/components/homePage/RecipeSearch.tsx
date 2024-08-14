65c383836fb2ccc1804a1d5505b91c83043be50b
hanna <hannadhm@hotmail.co.uk>
2024-08-14 16:33:08 +0100

    implement api request using axios

--- app/components/homePage/RecipeSearch.tsx
+++ app/components/homePage/RecipeSearch.tsx
@@ -2,15 +2,16 @@ import RecipeSection from "./RecipieSection";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
 import data from "../../api/endpoint-test";
+import axios from "axios";
+
 // create .env file for keys!!
 
 const RecipeSearch = () => {
   const [recipes, setRecipes] = useState(data.recipes);
+  const [showResults, setShowResults] = useState(false);
 
   // handle search async function: takes a url and runs through if statements to select filters
-  // const options = headers for key
-  // try/catch for data response
-  // try -> success -> await axios.get(url, options) setSearchState(true)
+
   // if data is returned, save data in state array (extract data here) setRecipes(response.recipes)
 
   // searchbar props: onSearch={handleSearch}, errorMessage={catch message}
@@ -23,9 +24,58 @@ const RecipeSearch = () => {
 
   //handle addToFavourites
 
-  const handleSearch = async (searchParams) => {
-    console.log("in handle search");
+  const handleSearch = async (
+    searchBy,
+    input,
+    category,
+    cuisine,
+    diet,
+    intolerances,
+  ) => {
+    const headers = {
+      "x-rapidapi-key": "06c5b738f4mshc8d3980a4797856p1bc186jsn6b1d2e762eba",
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    };
+
+    const url =
+      searchBy === "recipe name"
+        ? "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+        : "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients";
+
+    const options =
+      searchBy === "recipe name"
+        ? {
+            method: "GET",
+            url: url,
+            params: {
+              query: input,
+              cuisine: cuisine,
+              diet: diet,
+              intolerances: intolerances,
+              type: category,
+              number: "1",
+            },
+            headers: headers,
+          }
+        : {
+            method: "GET",
+            url: url,
+            params: {
+              ingredients: `${input.replace(" ", ",")}`,
+              number: "1",
+            },
+            headers: headers,
+          };
+    try {
+      const response = await axios.request(options);
+      setShowResults(true);
+      setRecipes(response.data);
+      console.log("data ---> ", response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
     <div>
       <SearchBar onSearch={handleSearch} errorMessage={"error"} />
